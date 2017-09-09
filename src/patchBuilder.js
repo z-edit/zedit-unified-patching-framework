efe989ec6c38acdd58288a840289a799a9ff2ed5
Mator <why101@msn.com>
2017-09-09 11:02:24 -0700

    disabled RemoveIdenticalRecords until it's fixed

--- src/patchBuilder.js
+++ src/patchBuilder.js
@@ -90,7 +90,8 @@ ngapp.service('patchBuilder', function(patcherService) {
     };
 
     this.cleanPatchFile = function(patchFile) {
-        xelib.RemoveIdenticalRecords(patchFile);
+        // TODO: uncomment when fixed
+        // xelib.RemoveIdenticalRecords(patchFile);
         xelib.CleanMasters(patchFile);
     };
 
