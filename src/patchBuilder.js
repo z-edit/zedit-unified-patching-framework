3b986b3f25b00c3488adbddbc6f86c9d65463992
Mator <why101@msn.com>
2017-09-11 00:43:27 -0700

    updated patchBuilder to run patchers with the patchWorker in the background

--- src/patchBuilder.js
+++ src/patchBuilder.js
@@ -1,91 +1,90 @@
-ngapp.service('patchBuilder', function(patcherService) {
-    let cache = {};
+ngapp.service('patchBuilder', function($rootScope, $q, patcherService, errorService, backgroundService) {
+    let progress, cache = {};
 
-    // private functions
-    let getOrCreatePatchRecord = function(patchFile, record) {
-        return xelib.AddElement(patchFile, xelib.GetHexFormID(record));
-    };
-
-    let getFile = function(filename) {
-        if (!cache[filename]) {
-            cache[filename] = { handle: xelib.FileByName(filename) };
-        }
-        return cache[filename];
-    };
-
-    let getRecords = function(filename, search, includeOverrides) {
-        let file = getFile(filename),
-            cacheKey = `${search}_${+includeOverrides}`;
-        if (!file[cacheKey]) {
-            file[cacheKey] = xelib.GetRecords(file.handle, search, includeOverrides);
-        }
-        return file[cacheKey];
-    };
-
-    let getPatcherHelpers = function(patcher, filesToPatch) {
-        return {
-            LoadRecords: function(search, includeOverrides = false) {
-                return filesToPatch.reduce(function(records, filename) {
-                    return records.concat(getRecords(filename, search, includeOverrides));
-                }, []);
-            },
-            AllSettings: patcherService.settings
-            // TODO: More helpers here?
-        }
-    };
-
-    let getRecordsToPatch = function(load, filename, settings, locals) {
-        let plugin = getFile(filename),
-            loadOpts = load(plugin.handle, settings, locals);
-        if (!loadOpts) return [];
-        let records = getRecords(filename, loadOpts.signature, false);
-        return loadOpts.filter ? records.filter(loadOpts.filter) : records;
-    };
-
-    let executeProcessBlock = function(processBlock, patchFile, filesToPatch, settings, locals) {
-        let load = processBlock.load,
-            patch = processBlock.patch;
-        filesToPatch.forEach(function(filename) {
-            let recordsToPatch = getRecordsToPatch(load, filename, settings, locals);
-            if (recordsToPatch.length === 0) return;
-            recordsToPatch.forEach(function(record) {
-                let patchRecord = getOrCreatePatchRecord(patchFile, record);
-                patch(patchRecord, settings, locals);
-            });
+    let getMaxProgress = function(patchPlugin) {
+        return patchPlugin.patchers.reduce(function(sum, patcher) {
+            let numProcessTasks = 3 * patcher.process.length;
+            return sum + 3 + numProcessTasks * patcher.filesToPatch.length;
         });
+    };
+
+    let build = function(patchPlugin) {
+        return backgroundService.run({
+            filename: `${modulePath}/patchWorker.js`,
+            data: {
+                cache: cache,
+                patchPlugin: patchPlugin,
+                settings: patcherService.settings
+            },
+            // callback functions are pascal case for clarity
+            callbacks: {
+                LogMessage: function(message, level) {
+                    if (!progress) return;
+                    $rootScope.$applyAsync(function() {
+                        progress.log.push({ message: message, level: level });
+                    });
+                },
+                ProgressMessage: function(msg) {
+                    $rootScope.$applyAsync(() =>  progress.message = msg);
+                },
+                AddProgress: function(num) {
+                    $rootScope.$applyAsync(() =>  progress.current += num);
+                },
+                ProgressTitle: function(title) {
+                    $rootScope.$applyAsync(() =>  progress.title = title);
+                },
+                SetCache: (_cache) => cache = _cache
+            }
+        });
+    };
+
+    let cleanup = function() {
+        cache = {};
+        xelib.FreeHandleGroup();
+        $rootScope.$broadcast('closeModal');
+        $rootScope.$broadcast('fileAdded');
+    };
+
+    let wrapPatchers = function(callback, maxProgress) {
+        xelib.CreateHandleGroup();
+        openProgressModal(maxProgress);
+        callback().then(cleanup, function(error) {
+            cleanup();
+            errorService.handleException(error);
+        });
+    };
+
+    let getActivePatchPlugins = function(patchPlugins) {
+        return patchPlugins.filter(function(patchPlugin) {
+            return !patchPlugin.disabled;
+        })
+    };
+
+    let getTotalMaxProgress = function(patchPlugins) {
+        return patchPlugins.reduce(function (sum, patchPlugin) {
+            return sum + getMaxProgress(patchPlugin);
+        })
+    };
+
+    let buildNextPatchPlugin = function(index, patchPlugins, action) {
+        if (index === patchPlugins.length) {
+            action.resolve();
+            return;
+        }
+        build(patchPlugins[index]).then(
+            () => buildNextPatchPlugin(index + 1, patchPlugins, action),
+            (error) => action.reject(error)
+        );
     };
 
     // public functions
-    this.executePatcher = function(scope, patcherId, filesToPatch, patchFile) {
-        let patcher = patcherService.getPatcher(patcherId),
-            exec = patcher.execute,
-            settings = patcherService.settings[patcherId],
-            helpers = getPatcherHelpers(patcher, filesToPatch),
-            locals = {};
-
-        exec.initialize && exec.initialize(patchFile, helpers, settings, locals);
-        exec.process && exec.process.forEach(function(processBlock) {
-            executeProcessBlock(processBlock, patchFile, filesToPatch, settings, locals);
-        });
-        exec.finalize && exec.finalize(patchFile, helpers, settings, locals);
+    this.buildPatchPlugins = function(patchPlugins) {
+        let activePatchPlugins = getActivePatchPlugins(patchPlugins),
+            maxProgress = getTotalMaxProgress(activePatchPlugins);
+        wrapPatchers(function() {
+            let action = $q.defer();
+            buildNextPatchPlugin(0, activePatchPlugins, action);
+            return action.promise;
+        }, maxProgress);
     };
-
-    this.preparePatchFile = function(filename) {
-        if (!xelib.HasElement(0, filename)) {
-            let dataPath = xelib.GetGlobal('DataPath');
-            fh.jetpack.cwd(dataPath).remove(filename);
-        }
-        let patchFile = xelib.AddElement(0, filename);
-        xelib.NukeFile(patchFile);
-        xelib.AddAllMasters(patchFile);
-        return patchFile;
-    };
-
-    this.cleanPatchFile = function(patchFile) {
-        // TODO: uncomment when fixed
-        // xelib.RemoveIdenticalRecords(patchFile);
-        xelib.CleanMasters(patchFile);
-    };
-
-    this.clearCache = () => cache = {};
 });
