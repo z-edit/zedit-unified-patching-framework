7c098d57343411eeb6170a4d92a17869fffa9200
Mator <why101@msn.com>
2017-09-11 00:42:16 -0700

    now registering deferred module loader for UPF

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-/* global ngapp, xelib */
+/* global ngapp, xelib, moduleService */
 // helper variables and functions
 const modulePath = '../modules/unifiedPatchingFramework';
 const openManagePatchersModal = function(scope) {
-  scope.$emit('openModal', 'managePatchers', {
-    templateUrl: `${modulePath}/partials/managePatchersModal.html`
-  });
+  scope.$emit('openModal', 'managePatchers', { basePath: modulePath });
 };
 //=require src/*.js
 // add manage patchers context menu item to tree view context menu
@@ -39,4 +37,13 @@ ngapp.run(function($rootScope, patcherService) {
         patcherService.updateForGameMode(selectedProfile.gameMode);
     });
     $rootScope.$on('filesLoaded', patcherService.loadSettings);
+});
+
+// register deferred module loader
+moduleService.deferLoader('UPF');
+ngapp.run(function(patcherService) {
+    moduleService.registerLoader('UPF', function(module, fh) {
+        let fn = new Function('registerPatcher', 'fh', 'info', module.code);
+        fn(patcherService.registerPatcher, fh, module.info);
+    });
 });
