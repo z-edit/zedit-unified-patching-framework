518087160a34da995288502c7a7a234d5591f6de
Mator <why101@msn.com>
2017-09-14 00:37:57 -0700

    settings modal now binds banner image using modulePath

--- src/upfSettings.js
+++ src/upfSettings.js
@@ -1,4 +1,8 @@
 ngapp.controller('upfSettingsController', function($timeout, $scope) {
+    $scope.bannerStyle = {
+        background: `url('${modulePath}}/images/banner.jpg')`
+    };
+
     $scope.managePatchers = function() {
         $scope.saveSettings(false);
         $timeout(() => openManagePatchersModal($scope));
