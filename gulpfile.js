f34eccb4a73319fa2f13770ef394ae3e1ab38b11
Colin <why101@msn.com>
2018-02-07 15:51:05 -0800

    v1.1 release

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
-let gulp = require('gulp'),
-    clean = require('gulp-clean'),
-    include = require('gulp-include');
+const fs = require('fs'),
+      gulp = require('gulp'),
+      clean = require('gulp-clean'),
+      include = require('gulp-include'),
+      rename = require('gulp-rename'),
+      zip = require('gulp-zip');
 
 gulp.task('clean', function() {
     return gulp.src('dist', {read: false})
@@ -24,6 +27,20 @@ gulp.task('build', ['clean'], function() {
 
     gulp.src('module.json')
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('release', function() {
+    let moduleInfo = JSON.parse(fs.readFileSync('module.json')),
+        moduleId = moduleInfo.id,
+        moduleVersion = moduleInfo.version,
+        zipFileName = `${moduleId}-v${moduleVersion}.zip`;
+
+    console.log(`Packaging ${zipFileName}`);
+
+    gulp.src('dist/**/*', { base: 'dist/'})
+        .pipe(rename((path) => path.dirname = `${moduleId}/${path.dirname}`))
+        .pipe(zip(zipFileName))
+        .pipe(gulp.dest('.'));
 });
 
 gulp.task('watch', function() {
