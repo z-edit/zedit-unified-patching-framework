7fabd7929f0ccf6e47f6e2b91b7d582839cc1ebf
Colin <why101@msn.com>
2019-06-24 12:03:56 -0700

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,49 @@
 const fs = require('fs'),
       gulp = require('gulp'),
       clean = require('gulp-clean'),
-      include = require('gulp-include'),
       rename = require('gulp-rename'),
       zip = require('gulp-zip');
 
+let files = ['index.js', 'module.json', 'LICENSE', 'README.md'];
+let folders = ['src/**/*.js', 'partials/*.html', 'images/*', 'docs/*'];
+
+let loadModuleInfo = function() {
+    let text = fs.readFileSync('module.json', 'utf8');
+    return JSON.parse(text);
+};
+
 gulp.task('clean', function() {
-    return gulp.src('dist', {read: false})
-        .pipe(clean());
+    return gulp.src('dist', { read: false }).pipe(clean());
 });
 
 gulp.task('build', gulp.series('clean', function(done) {
-    gulp.src('index.js')
-        .pipe(include())
-        .on('error', console.log)
-        .pipe(gulp.dest('dist'));
+    files.forEach(filename => {
+        gulp.src(filename).pipe(gulp.dest('dist'));
+    });
 
-    gulp.src('partials/*.html')
-        .pipe(gulp.dest('dist/partials'));
-
-    gulp.src('images/*')
-        .pipe(gulp.dest('dist/images'));
-
-    gulp.src('docs/*')
-        .pipe(gulp.dest('dist/docs'));
-
-    gulp.src('module.json')
-        .pipe(gulp.dest('dist'));
+    folders.forEach(path => {
+        let name = path.split('/').shift();
+        gulp.src(path).pipe(gulp.dest(`dist/${name}`));
+    });
 
     done();
 }));
 
 gulp.task('release', function() {
-    let moduleInfo = JSON.parse(fs.readFileSync('module.json')),
-        moduleId = moduleInfo.id,
-        moduleVersion = moduleInfo.version,
-        zipFileName = `${moduleId}-v${moduleVersion}.zip`;
+    let {id, version} = loadModuleInfo(),
+        zipFileName = `${id}-v${version}.zip`;
 
     console.log(`Packaging ${zipFileName}`);
 
-    gulp.src('dist/**/*', { base: 'dist/'})
-        .pipe(rename((path) => path.dirname = `${moduleId}/${path.dirname}`))
+    return gulp.src('dist/**/*', { base: 'dist/'})
+        .pipe(rename((path) => path.dirname = `${id}/${path.dirname}`))
         .pipe(zip(zipFileName))
         .pipe(gulp.dest('.'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('src/**/*.js', ['build']);
-    gulp.watch('partials/**/*.html', ['build']);
-    gulp.watch('images/**/*', ['build']);
-    gulp.watch('module.json', ['build']);
+    files.forEach(filename => gulp.watch(filename, ['build']));
+    folders.forEach(folder => gulp.watch(folder, ['build']));
 });
 
 gulp.task('default', gulp.series('build', 'watch'));
